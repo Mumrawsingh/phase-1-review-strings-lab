448b1d4e7d7895fbf5ebeed51bdf469c21f21472
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2021-11-03 01:22:28 -0400

    commit tree 9a8ffdf07dbd70b15f77c5e3d4f4c1b720dfb4b4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = "Matthew U"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = `"WELCOME TO FLATBOOK, MATTHEW U"!`
+
+const shortGreeting = "Welcome, M!"
